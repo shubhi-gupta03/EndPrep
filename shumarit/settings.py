00ae6ac98fc1c2f58821f018b9fdf717cf347a2d
Umar Khursheed <137692710+umarsayed12@users.noreply.github.com>
2024-02-23 01:08:00 +0530

    Update settings.py

--- shumarit/settings.py
+++ shumarit/settings.py
@@ -18,7 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z2cw&$4y+pw-zj-72_nml_xtwuravx-wu6r@one!t2zk5_d@*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
